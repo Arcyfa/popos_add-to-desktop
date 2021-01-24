e43547fda713c8f4c0715a85556ed0f76a139282
Maarten <Arcyfa@users.noreply.github.com>
2021-01-24 19:00:01 +0100

    Create add-to-desktop.sh

---
+++ usr/bin/add-to-desktop.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+if [ ${1} = "" ]; then
+	for f in /usr/share/applications/* ; do 
+		if grep -q "Actions=" $f
+		then
+			if grep -q "Add-To-Desktop" $f
+			then
+				##echo $f "has Actions. All ready processed"
+			else
+				##echo $f "has Actions"
+				sudo sed -i "s/Actions=/Actions=Add-To-Desktop;/g" $f
+				sudo echo "" | sudo tee -a $f > /dev/null
+				sudo echo "[Desktop Action Add-To-Desktop]" | sudo tee -a $f > /dev/null
+				sudo echo "Name=Add to Desktop" | sudo tee -a $f > /dev/null
+				sudo echo "Exec=/usr/bin/add-to-desktop.sh \"$f\"" | sudo tee -a $f > /dev/null
+			fi
+		else
+			sudo echo "Actions=Add-To-Desktop;" | sudo tee -a $f > /dev/null
+			sudo echo "" | sudo tee -a $f > /dev/null
+			sudo echo "[Desktop Action Add-To-Desktop]" | sudo tee -a $f > /dev/null
+			sudo echo "Name=Add to Desktop" | sudo tee -a $f > /dev/null
+			sudo echo "Exec=/usr/bin/add-to-desktop.sh \"$f\"" | sudo tee -a $f > /dev/null
+		fi
+	done
+
+else
+	file=$(basename ${1})
+	cp "${1}" "/home/$USER/Desktop/"
+	chmod 755 "/home/$USER/Desktop/$file"
+fi
